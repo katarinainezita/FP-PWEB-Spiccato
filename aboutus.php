a4848e1dd0f4d6eabdf37e60dca0f938fca3939c
Katarina Inezita <inezitakatarina16@gmail.com>
2023-12-13 16:29:23 +0700

    add: about us

---
+++ aboutus.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <style type="text/css">
+            h5{
+                color: blue;
+            }
+            .p1{
+                text-align: justify;
+            }
+        </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>About us | Spiccato</title>
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+<body>
+<?php
+include 'includes/header.php';
+?>
+<div class="container" id="content">
+    <img src="img/43.png" style="float: left; height: 200px; margin-top:20px">
+    <div class="row">
+        <div class="col-lg-4" align="justify">
+            <h3>WHO WE ARE</h3>
+            <p id="p1">Spiccato is an online retail store specializing in the sale of a curated collection of fashion items, including t-shirts, shoes, and bags. The brand distinguishes itself through a commitment to quality, style, and innovation in the fashion industry. The name Spiccato suggests a focus on precision and uniqueness, mirroring the brand's dedication to providing distinctive and well-crafted products.
+<br><br>
+<h4>Brand Philosophy: </h4>Spiccato embraces a philosophy of "Building the Future," signifying not only a commitment to current fashion trends but also an aspiration to shape the future of the fashion industry. The brand seeks to inspire customers to embrace a style that is not only chic and contemporary but also sustainable and forward-thinking.
+<br><br>
+In summary, Spiccato is more than an online shop; it is a destination for fashion enthusiasts seeking a blend of style, quality, and innovation across t-shirts, shoes, and bags.</p>
+        </div>
+        <div class="col-lg-4" align="justify">
+            <h3>BUILDING THE FUTURE</h3>
+            <p id="p1"><h4>Innovative Shopping Experience:</h4>
+Recognizing the pivotal role of the online shopping experience, Spiccato has designed a user-friendly platform. It is easy to navigate and incorporates innovative features such as personalized options, style guides, and customer reviews to facilitate informed decision-making.
+<br><br>
+<h4>Environmental Commitment:</h4>
+Spiccato is dedicated to contributing to sustainability by integrating environmental considerations throughout its production chain. The use of eco-friendly materials and responsible production practices align with Spiccato's integral values and principles.
+
+<br><br>
+<h4>"Building the Future" Spirit:</h4>
+Spiccato transcends being merely an online store; it is a catalyst for change in the fashion industry. With the "Building the Future" spirit, the brand assists customers in constructing a sustainable and inspirational future style.
+<br><br>
+Spiccato aims not only to sell products but to shape a progressive and responsible future for fashion, setting the standard for ethical practices and innovative trends in the industry.</p>
+        </div>
+    </div>
+  </div>
+</div>
+<?php
+include 'includes/footer.php';
+?>
+</body>
+</html>
