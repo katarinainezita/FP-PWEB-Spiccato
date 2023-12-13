7e1a8fdc9b3f702a5657a441d6a2f0cc259ce31b
Katarina Inezita <inezitakatarina16@gmail.com>
2023-12-13 16:33:57 +0700

    add: product

---
+++ products.php
@@ -0,0 +1,362 @@
+<?php
+//establish the connection to database, and start the session
+require("includes/common.php");
+?>
+
+<!--Specifies document type is html-->
+<!DOCTYPE html>
+<!--Specifies the language as English-->
+<html lang="en">
+    <head>
+        <!--instructs browser on how to control the page's dimensions and scaling-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--Title of products page-->
+        <title>Products | Spiccato</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <?php
+        include 'includes/header.php';
+        include 'includes/check-if-added.php';
+        ?>
+        <div class="container" id="content">
+            <!-- Jumbotron Header -->
+            <div class="jumbotron home-spacer" id="products-jumbotron">
+                <h1>Welcome to Spiccato!</h1>
+                <p>We have the best T-shirt, Bag, and Shoes.</p>
+
+            </div>
+            <hr>
+
+            <div class="row text-center" id="cameras">
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/28.png" alt="">
+                        <div class="caption">
+                            <h3>Elegance Creations </h3>
+                            <p>Price: Rp. 1.500.000 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(1)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=1" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/38.png" alt="">
+                        <div class="caption">
+                            <h3>Urban Explorer </h3>
+                            <p>Price: Rp. 1.000.000 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(2)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=2" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/39.png" alt="">
+                        <div class="caption">
+                            <h3>Luxury Trends</h3>
+                            <p>Price: Rp 2.500.000</p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(3)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=3" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/42.png" alt="">
+                        <div class="caption">
+                            <h3>Tech Gear</h3>
+                            <p>Price: Rp 1.800.000</p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(4)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=4" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row text-center" id="watches">
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/36.png" alt="">
+                        <div class="caption">
+                            <h3>StrideLux Footwear </h3>
+                            <p>Price: Rp 500.000 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(5)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=5" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/40.png" alt="">
+                        <div class="caption">
+                            <h3>UrbanKicks </h3>
+                            <p>Price: Rp 650.000 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(6)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=6" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/37.png" alt="">
+                        <div class="caption">
+                            <h3>EleganceSoles</h3>
+                            <p>Price: Rp 800.000 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(7)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=7" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/27.png" alt="">
+                        <div class="caption">
+                            <h3>AdventureTrek</h3>
+                            <p>Price: Rp 750.000 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(8)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=8" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row text-center" id="shirts">
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/33.png" alt="">
+                        <div class="caption">
+                            <h3> Casual Threads</h3>
+                            <p>Price: Rp 150.000 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(9)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=9" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/34.png" alt="">
+                        <div class="caption">
+                            <h3>Street Vibe Wear</h3>
+                            <p>Price: Rp 120.000</p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(10)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=10" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/35.png" alt="">
+                        <div class="caption">
+                            <h3>Urban Elegance</h3>
+                            <p>Price: Rp 180.000</p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(11)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=11" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/41.png" alt="">
+                        <div class="caption">
+                            <h3>Tech Chic Clothing</h3>
+                            <p>Price Rp 200.000</p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(12)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=12" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <hr>
+        </div>
+
+        <?php include("includes/footer.php"); ?>
+    </body>
+
+</html>
